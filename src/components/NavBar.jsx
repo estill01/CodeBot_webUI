c024590206350ff9bd0ed4ed75f496a98d014982
estill01 <ehs.stillman@gmail.com>
2023-02-16 15:03:27 -0800

    Add: nav logo + styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import GeneratorInput from './Generator';
+import LogoBot from '../assets/logo_bot/logo_bot.png';
 
 const NavBar = ({ className }) => {
   return (
@@ -8,9 +9,21 @@ const NavBar = ({ className }) => {
         id='NavBar' 
         className={`fixed top-0 left-0 right-0 h-16 flex flex-row text-white p-4 ${className}`}
       >
+        <Logo/>
         <GeneratorInput className='mx-auto self-center'/>
       </div>
     </div>
   )
 };
 export default NavBar;
+
+const Logo = ({className}) => {
+  return (
+    <div className={`flex flex-row content-center text-xl ${className}`}>
+      <div className='my-auto mr-1 text-[#00f100] font-medium tracking-[-0.1em]'>
+        &lt;/&gt;
+      </div>
+      <div className='my-auto font-semibold'>Code<span className='font-normal'>Bot</span></div>
+    </div>
+  )
+}
